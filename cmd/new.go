b139405074c8edbf0c6383716226b826bb160bcc
elliot <ellix260@gmail.com>
2022-01-03 19:18:53 +0800

    Add markdown mode support.

--- cmd/new.go
+++ cmd/new.go
@@ -16,6 +16,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var is_markdown_mode bool
+
 // newCmd represents the new command
 var newCmd = &cobra.Command{
 	Use:   "new COMMAND",
@@ -59,11 +61,17 @@ func scan(message string) (string, error) {
 		if line == "" {
             continue
 		}
-        cmds = append(cmds, line)
-        if strings.HasSuffix(line, "\\") {
-            l.SetPrompt(color.YellowString("Command> "))
-            continue
-        }
+	if is_markdown_mode {
+	    if line != "eof" && line != "EOF" {
+		line += " \\"
+		cmds = append(cmds, line)
+		l.SetPrompt(color.YellowString("Command> "))
+		continue
+	    }
+	} else {
+	    cmds = append(cmds, line)
+	}
+
         var finalCmd string
         if strings.HasPrefix(cmds[0], "#") {
             finalCmd = strings.Join(cmds, "\n\n")
@@ -73,6 +81,91 @@ func scan(message string) (string, error) {
 		return finalCmd, nil
 	}
 	return "", errors.New(color.RedString("canceled..."))
+}
+
+func scan_desc(message string) (string, error) {
+	var desc string = ""
+	tempFile := "/tmp/pet.tmp"
+	if runtime.GOOS == "windows" {
+		tempDir := os.Getenv("TEMP")
+		tempFile = filepath.Join(tempDir, "pet.tmp")
+	}
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:          message,
+		HistoryFile:     tempFile,
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+	})
+	if err != nil {
+		return "", err
+	}
+	defer l.Close()
+	for {
+		desc, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(desc) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
+		}
+
+		desc = strings.TrimSpace(desc)
+
+		if desc == "" {
+			l.SetPrompt(color.GreenString("Description> "))
+			continue
+		}
+
+		return desc, nil
+	}
+	return desc, nil
+}
+
+func scan_mode(message string) (bool, error) {
+	tempFile := "/tmp/pet.tmp"
+	if runtime.GOOS == "windows" {
+		tempDir := os.Getenv("TEMP")
+		tempFile = filepath.Join(tempDir, "pet.tmp")
+	}
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:          message,
+		HistoryFile:     tempFile,
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+	})
+	if err != nil {
+		return false, err
+	}
+	defer l.Close()
+	for {
+		line, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
+		}
+
+		line = strings.TrimSpace(line)
+		if line == "" {
+	            l.SetPrompt(color.MagentaString("Mode> "))
+		    continue
+		}
+		if line == "m" {
+			return true, nil
+		} else if line == "n" {
+			return false, nil
+		} else {
+			break
+		}
+	}
+	return false, errors.New(color.RedString("Choose between 'm' and 'n'..."))
 }
 
 func new(cmd *cobra.Command, args []string) (err error) {
@@ -89,12 +182,16 @@ func new(cmd *cobra.Command, args []string) (err error) {
 		command = strings.Join(args, " ")
 		fmt.Fprintf(color.Output, "%s %s\n", color.YellowString("Command>"), command)
 	} else {
+		is_markdown_mode, err = scan_mode(color.MagentaString("Mode> "))
+		if err != nil {
+			return err
+		}
 		command, err = scan(color.YellowString("Command> "))
 		if err != nil {
 			return err
 		}
 	}
-	description, err = scan(color.GreenString("Description> "))
+	description, err = scan_desc(color.GreenString("Description> "))
 	if err != nil {
 		return err
 	}
